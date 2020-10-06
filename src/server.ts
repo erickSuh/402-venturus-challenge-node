828dcc76061c540f6390c2c01249ce7e5164dcbc
Érick Kenji Sugahara <erick.sugahara@gmail.com>
2020-10-06 18:17:26 -0300

    add eslint, prettier e jest

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express = require("express");
+import express from 'express'
+// import morgan from 'morgan'
 // Create a new express app instance
-const app: express.Application = express();
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
-app.listen(3000, function () {
-  console.log("App is listening on port 3000!");
-});
+const app: express.Application = express()
+
+// app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('App is listening on port 3000!')
+})
